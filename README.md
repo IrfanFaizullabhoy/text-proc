042a39f7126109d627924fd1c0a41d5fb82ce5b9
Irfan Faizullabhoy <supercurry911@gmail.com>
2017-11-27 08:21:04 -0700

    finishing up

---
+++ README.md
@@ -0,0 +1,12 @@
+text-proc
+---------
+
+**run instructions**
+
+`docker-compose build`
+`docker-compose up`
+
++ Golang 1.7
++ Docker
++ Alpine Linux
++ Vendoring
--- main.go
+++ main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"bufio"
-	"encoding/csv"
 	"errors"
 	"fmt"
-	"golang.org/x/sync/syncmap"
-	"io"
 	"log"
 	"math"
 	"os"
+	"path/filepath"
+	"strconv"
 	"strings"
 	"sync"
+	"text/tabwriter"
+	"time"
+
+	"golang.org/x/sync/syncmap"
 )
 
+//STRUCTS
 type medianStorageArray struct {
 	mutex          sync.RWMutex
 	size           int
@@ -36,14 +40,180 @@ type lineDuplicateMap struct {
 	numDupes int
 }
 
-func NewWriter(w io.Writer) (writer *csv.Writer) {
-	writer = csv.NewWriter(w)
-	writer.Comma = '\t'
-	return writer
+type Stopwatch struct {
+	start, stop time.Time
 }
 
-// O(N), where N is at most 4000
+func check(err error) {
+	if err != nil {
+		fmt.Errorf(err.Error())
+	}
+}
+
+func main() {
+
+	//SETUP
+	waitGroup := sync.WaitGroup{}
+	channelWaitGroup := sync.WaitGroup{}
+
+	lineChan := make(chan string)
+	keyWordStorage := keyWordStorage{}
+	tokenMedian := medianStorageArray{}
+	lineMedian := medianStorageArray{}
+	tokenStdDev := stdVarianceCalculator{}
+	lineStdDev := stdVarianceCalculator{}
+	lineDupMap := lineDuplicateMap{}
+
+	keyWordStorage.InitializeKeywords("keywords.txt")
+
+	//PROCESS TEXT FILES
+	searchDir := "text-files/"
+	var fileList []string
+	err := filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
+		fileList = append(fileList, path)
+		return nil
+	})
+	check(err)
+	for _, file := range fileList {
+		if strings.HasSuffix(file, ".txt") {
+			channelWaitGroup.Add(1)
+			go processFile(file, lineChan, &channelWaitGroup)
+		}
+	}
+	go func() {
+		channelWaitGroup.Wait()
+		close(lineChan)
+	}()
+	for line := range lineChan {
+		lineLen := len(line)
+		waitGroup.Add(3)
+		go lineDupMap.CheckForDuplicate(line, &waitGroup)
+		go lineStdDev.AddValue(lineLen, &waitGroup)
+		go lineMedian.AddToMedian(lineLen, &waitGroup)
+
+		for _, token := range strings.Fields(line) {
+			tokenLen := len(token)
+			waitGroup.Add(3)
+			go tokenStdDev.AddValue(tokenLen, &waitGroup)
+			go tokenMedian.AddToMedian(tokenLen, &waitGroup)
+			go keyWordStorage.CheckForKeywords(token, &waitGroup)
+		}
+	}
+
+	waitGroup.Wait()
+
+	// COLLECT DATA -> WRITE TO RESULTS FILE
+
+	d := lineDupMap.NumDuplicates()
+	lm, err := lineMedian.returnMedian()
+	check(err)
+	ls, err := lineStdDev.returnStdDev()
+	check(err)
+	tm, err := tokenMedian.returnMedian()
+	check(err)
+	ts, err := tokenStdDev.returnStdDev()
+	check(err)
+	keywords := keyWordStorage.ReturnKeywords()
+
+	dString := strconv.Itoa(d)
+	lmString := strconv.FormatFloat(float64(lm), 'f', -1, 64)
+	lsString := strconv.FormatFloat(ls, 'f', -1, 64)
+	tmString := strconv.FormatFloat(float64(tm), 'f', -1, 64)
+	tsString := strconv.FormatFloat(ts, 'f', -1, 64)
+	var keyWordKeys string
+	for _, key := range keywords {
+		keyWordKeys += key[0] + "\t"
+	}
+	var keyWordFrequences string
+	for _, freq := range keywords {
+		keyWordFrequences += freq[1] + "\t"
+	}
+
+	resultsFile, err := os.Create("results.tsv")
+	check(err)
+
+	w := new(tabwriter.Writer)
+	w.Init(resultsFile, 0, 4, 0, '\t', 0)
+	fmt.Fprintln(w, "d\tlm\tls\ttm\tts\t"+keyWordKeys)
+	fmt.Fprintln(w, dString+"\t"+lmString+"\t"+lsString+"\t"+tmString+"\t"+tsString+"\t"+keyWordFrequences)
+	w.Flush()
+	resultsFile.Close()
+}
+
+//KEYWORD FUNCTIONALITY CODE
+
+//InitializeKeywords initializes a map with the keywords from a specified file
+func (ks *keyWordStorage) InitializeKeywords(filename string) {
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		line = strings.ToLower(line)
+		ks.Keywords.Store(line, 0)
+	}
+}
+
+//CHeckForKeywords is a streaming function that uses a syncmap to check if a word is a keyword O(1)
+func (ks *keyWordStorage) CheckForKeywords(word string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	word = strings.ToLower(word)
+	val, ok1 := ks.Keywords.Load(word)
+	if ok1 {
+		num, ok2 := val.(int)
+		if ok2 {
+			ks.Keywords.Store(word, num+1)
+		}
+	}
+}
+
+//ReturnKeywords returns all the keywords and frequencies - O(N) where N is the number of keyword results (lower bound)
+func (ks *keyWordStorage) ReturnKeywords() [][]string {
+	var returnArray [][]string
+	ks.Keywords.Range(func(k, v interface{}) bool {
+		returnArray = append(returnArray, []string{k.(string), strconv.Itoa(v.(int))})
+		return true
+	})
+	return returnArray
+}
+
+//CheckForDuplicate takes in any given line, and checks if it has been seen before - O(1)
+func (ldm *lineDuplicateMap) CheckForDuplicate(line string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	_, exists := ldm.Lines.LoadOrStore(line, 1)
+	if exists {
+		ldm.numDupes++
+	}
+}
+
+//NumDuplicates returns the number of duplicate lines seen over time in O(1)
+func (ldm *lineDuplicateMap) NumDuplicates() int {
+	return ldm.numDupes
+}
+
+//AddToMedian takes any given value and adds it to the Map, so that median can be calculated quickly in the future - O(1)
+func (ms *medianStorageArray) AddToMedian(num int, wg *sync.WaitGroup) error {
+	ms.mutex.Lock()
+	defer ms.mutex.Unlock()
+	defer wg.Done()
+
+	if num >= 4000 {
+		return errors.New("index too large, line is too long")
+	}
+	ms.frequencyArray[num] = ms.frequencyArray[num] + 1
+	ms.size++
+	return nil
+}
+
+//returnMedian returns the median of values seen so far O(N), where N < 4000
 func (ms *medianStorageArray) returnMedian() (float32, error) {
+	start := time.Now()
+	fmt.Printf("Calculating Median took: %v\n", time.Since(start))
 
 	ms.mutex.Lock()
 	defer ms.mutex.Unlock()
@@ -90,27 +260,19 @@ func (ms *medianStorageArray) returnMedian() (float32, error) {
 	}
 }
 
+//nextBucket abstracts out logic from the returnMedian function, finding the next non-zero bucket to compute avg of two values
 func nextBucket(index int, array [4000]int) (int, error) {
 	index += 1
 	for index < len(array) {
 		if array[index] > 0 {
 			return index, nil
 		}
+		index++
 	}
 	return -1, errors.New("reached end of array")
 }
 
-func (svc *stdVarianceCalculator) returnVariance() (float32, error) {
-	svc.mutex.RLock()
-	defer svc.mutex.RUnlock()
-
-	if svc.iterations < 2 {
-		return -1, errors.New("Empty List")
-	} else {
-		return float32(svc.runningM2) / float32(svc.iterations-1), nil
-	}
-}
-
+//AddValue is a O(1) streaming algorithm that adds a value to a running approx of variance
 func (svc *stdVarianceCalculator) AddValue(x int, wg *sync.WaitGroup) {
 	svc.mutex.Lock()
 	defer svc.mutex.Unlock()
@@ -123,131 +285,23 @@ func (svc *stdVarianceCalculator) AddValue(x int, wg *sync.WaitGroup) {
 	svc.runningM2 += delta * delta2
 }
 
-func (ks *keyWordStorage) InitializeKeywords(filename string) {
+//returnStdDev is a O(1) algorithm that returns the standard deviation based on a set of numbers seen thus far
+func (svc *stdVarianceCalculator) returnStdDev() (float64, error) {
+	start := time.Now()
+	fmt.Printf("Calculating StdDev took: %v\n", time.Since(start))
 
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+	svc.mutex.RLock()
+	defer svc.mutex.RUnlock()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		line = strings.ToLower(line)
-		ks.Keywords.Store(line, 0)
-		fmt.Println(line)
-	}
-}
-
-func check(err error) {
-	if err != nil {
-		fmt.Errorf(err.Error())
+	if svc.iterations < 2 {
+		return -1, errors.New("Empty List")
+	} else {
+		variance := float64(svc.runningM2) / float64(svc.iterations-1)
+		return math.Sqrt(variance), nil
 	}
 }
 
-func main() {
-
-	waitGroup := sync.WaitGroup{}
-	channelWaitGroup := sync.WaitGroup{}
-
-	lineChan := make(chan string)
-	keyWordStorage := keyWordStorage{}
-	tokenMedian := medianStorageArray{}
-	lineMedian := medianStorageArray{}
-	tokenStdDev := stdVarianceCalculator{}
-	lineStdDev := stdVarianceCalculator{}
-	lineDupMap := lineDuplicateMap{}
-
-	keyWordStorage.InitializeKeywords("keywords.txt")
-	channelWaitGroup.Add(2)
-	go processFile("sample.txt", lineChan, &channelWaitGroup)
-	go processFile("text2.txt", lineChan, &channelWaitGroup)
-	go func() {
-		channelWaitGroup.Wait()
-		close(lineChan)
-	}()
-	for line := range lineChan {
-		lineLen := len(line)
-		waitGroup.Add(3)
-		go lineDupMap.CheckForDuplicate(line, &waitGroup)
-		go lineStdDev.AddValue(lineLen, &waitGroup)
-		go lineMedian.AddToMedian(lineLen, &waitGroup)
-
-		for _, token := range strings.Fields(line) {
-			tokenLen := len(token)
-			waitGroup.Add(3)
-			go tokenStdDev.AddValue(tokenLen, &waitGroup)
-			go tokenMedian.AddToMedian(tokenLen, &waitGroup)
-			go keyWordStorage.CheckForKeywords(token, &waitGroup)
-		}
-	}
-
-	waitGroup.Wait()
-	keyWordStorage.PrintKeywords()
-	ts, err := tokenStdDev.returnVariance()
-	fmt.Println(math.Sqrt(float64(ts)))
-	check(err)
-	dupes := lineDupMap.NumDuplicates()
-	fmt.Println(dupes)
-	tm, err := tokenMedian.returnMedian()
-	check(err)
-	fmt.Println(tm)
-	results, err := os.Create("results.tsv")
-	check(err)
-	defer results.Close()
-	writer := NewWriter(results)
-	writer.Write([]string{"one", "two", "five"})
-	writer.Write([]string{"three", "four"})
-	writer.Write([]string{"six", "seven"})
-	writer.Flush()
-}
-
-func (ks *keyWordStorage) CheckForKeywords(word string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	word = strings.ToLower(word)
-	val, ok1 := ks.Keywords.Load(word)
-	if ok1 {
-		num, ok2 := val.(int)
-		if ok2 {
-			ks.Keywords.Store(word, num+1)
-		}
-	}
-}
-
-func (ks *keyWordStorage) PrintKeywords() {
-	ks.Keywords.Range(func(k, v interface{}) bool {
-		fmt.Print(k.(string) + ": ")
-		fmt.Println(v.(int))
-		return true
-	})
-}
-
-func (ldm *lineDuplicateMap) NumDuplicates() int {
-	return ldm.numDupes
-}
-
-func (ldm *lineDuplicateMap) CheckForDuplicate(line string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	_, exists := ldm.Lines.LoadOrStore(line, 1)
-	if exists {
-		ldm.numDupes++
-	}
-}
-
-func (ms *medianStorageArray) AddToMedian(num int, wg *sync.WaitGroup) error {
-	ms.mutex.Lock()
-	defer ms.mutex.Unlock()
-	defer wg.Done()
-
-	if num >= 4000 {
-		return errors.New("index too large, line is too long")
-	}
-	ms.frequencyArray[num] = ms.frequencyArray[num] + 1
-	ms.size++
-	return nil
-}
-
+//processFile reads through a file, sending its contents to a channel, which is used to process data
 func processFile(filename string, lineChan chan string, channelWaitGroup *sync.WaitGroup) {
 	defer channelWaitGroup.Done()
 
@@ -258,10 +312,13 @@ func processFile(filename string, lineChan chan string, channelWaitGroup *sync.W
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	start := time.Now()
 	for scanner.Scan() {
 		line := scanner.Text()
 		lineChan <- line
 	}
+	elapsedTime := time.Since(start)
+	fmt.Printf("Reading in "+filename+" took: %v\n", elapsedTime)
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
